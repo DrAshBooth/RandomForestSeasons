eb6a08ab8b8116d5e8e3f5a7d8369e6d42823583
Ash Booth <ash.booth@soton.ac.uk>
2013-06-26 10:34:56 +0100

    more fleshing of barebones

--- src/prediction.py
+++ src/prediction.py
@@ -4,27 +4,44 @@ Created on 25 Jun 2013
 @author: Ash Booth
 
 '''
-
-import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from expert import Expert
 
 class Prediction(object):
-    def __init__(self, nef=5, max_experts=48):
+    def __init__(self, features, targets, window_size, nef, max_experts):
         self.date = None
         self.trading_days_seen=1
         self.start_trading_after=50
         self.experts = []
-        self.new_exp_freq = nef
+        self.window_size = window_size
+        self.nef = nef # new expert frequency
         self.max_experts = max_experts
         self.threshold = 0.2
         self.previeous_pred = 0.0
         self.prediction = 0.0
-        self.__loadData()
-    
-    def __loadData(self):
-        full_data = np.genfromtxt("stock_metrics.csv",delimiter=',')
+        self.features = features
+        self.targets = targets
         
-    def makePrediction(self, date):
+    def createExpert(self, row_index, verbose):
+        training_features = self.features[row_index-self.window_size:row_index,:]
+        training_targets = self.targets[row_index-self.window_size:row_index]
+        
+        # Generate classifer
+        rfr = RandomForestRegressor(n_estimators=10, n_jobs=-1)
+        if verbose: print "generating random forest regressor"
+        rfr.fit(training_features, training_targets)
+        # Add to list
+        self.experts.append(Expert(rfr, self.trading_days_seen, 
+                                   self.nef, len(self.experts)==0))
+        
+    def makePrediction(self, row_index, verbose):
+        
+        test_feature = self.features[row_index,:]
+        test_target = self.targets[row_index]
+        
+        self.previeous_pred = self.prediction
         # is it time to make a new expert
-        self.date = date
+        if self.trading_days_seen%self.nef==1:
+            self.createExpert(row_index, verbose)
         
         
--- src/sim.py
+++ src/sim.py
@@ -3,13 +3,26 @@ Created on 25 Jun 2013
 
 @author: Ash Booth
 '''
+from prediction import Prediction
+
+import numpy as np
+from sklearn import cross_validation as cv
 
 class Simulation:
     
-    def __init__(self, entry_dates, exit_dates, balance, start_date, end_date):
+    def __init__(self, balance, data_file, window_size = 50, 
+                 nef=5, max_experts=48):
+        self.nef = nef
+        self.window_size = window_size
+        self.max_experts = max_experts
         self.balance = balance
         self.profit = 0
         self.stock_held = 0
+        
+        # Data
+        self.data_file
+        self.features = None
+        self.targets = None
         
         # Stats
         self.returns = []
@@ -17,17 +30,24 @@ class Simulation:
         self.correct_buys
         self.correct_sells
         
+    def __loadData(self):
+        data = np.genfromtxt(self.data_file+".csv",delimiter=',')
+        self.features = data[:,:-1]
+        self.targets  = data[:,-1] 
+      
     def writeStats(self):
         # write the above
         # calc f-scores etc etc and write the
         pass
     
-    def run(self):
-        # for i in range(len(entry dates)):
+    def run(self, verbose):
+        # Training
+        predictor = Prediction(self.features, self.targets, 
+                               self.window_size, self.nef, self.max_experts)
+        for row_index in range(self.window_size,len(self.features)):
+            pred = predictor.makePrediction(row_index, verbose)
             # make prediction (including risk management)
-            # open position 
-            
-            # close position
+
             # update profit
             # update stats
         pass
